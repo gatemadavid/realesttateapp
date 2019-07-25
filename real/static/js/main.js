5d456baebbcb34ad1b7087677d9774a1b0f8683b
gatemadavid <dshefard@gmail.com>
2019-07-25 22:51:01 +0300

    change js for login and register forms

--- real/static/js/main.js
+++ real/static/js/main.js
@@ -1,6 +1,16 @@
 const date = new Date();
 document.querySelector(".year").innerHTML = date.getFullYear();
 
-setTimeout(function() {
+setTimeout(function () {
   $("#message").fadeOut("slow");
 }, 3000);
+
+$(".txtb input").on("focus", function () {
+  $(this).addClass("focus");
+
+});
+$(".txtb input").on("blur", function () {
+  if ($(this).val() == "")
+    $(this).removeClass("focus");
+
+});
